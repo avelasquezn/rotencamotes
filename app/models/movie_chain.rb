279ff7639f08fefd1c72fe6654a3ee073057974d
Angel Velasquez <angel@xendacentral.com>
2010-09-16 13:39:57 -0500

    Removed unused views and controllers

--- app/models/movie_chain.rb
+++ app/models/movie_chain.rb
@@ -1,4 +1,5 @@
 class MovieChain < ActiveRecord::Base
+  has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_many :theatres, :dependent => :destroy
   accepts_nested_attributes_for :theatres
   validates_presence_of :name
