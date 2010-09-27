f28256c14fbfd20dbed85f8c85d97e7b0e16b18b
Angel Velasquez <angel@xendacentral.com>
2010-09-27 14:28:12 -0500

    Updated profile show

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,6 +5,18 @@ class Profile < ActiveRecord::Base
   belongs_to :favorite_genre, :class_name => "Genre", :foreign_key => "favorite_genre_id"
   belongs_to :favorite_director, :class_name => "Person", :foreign_key => "favorite_director_id"
   belongs_to :favorite_writer, :class_name => "Person", :foreign_key => "favorite_writer_id"
+
+  def favorite_director_name
+    self.favorite_director.nil? ? 'No especificado' : self.favorite_director.name
+  end
+
+  def favorite_writer_name
+    self.favorite_writer.nil? ? 'No especificado' : self.favorite_writer.name
+  end
+
+  def favorite_actor_name
+    self.favorite_actor.nil? ? 'No especificado' : self.favorite_actor.name
+  end
 end
 
 
